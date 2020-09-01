0478fcbcd4bca42e6c34cba2731011d78485e742
Rahardyan Bisma <rahardyanbismasetya.putra@mitrais.com>
2020-09-01 08:25:39 +0700

    Initial Commit

---
+++ MovieApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by MTMAC36 on 01/09/20.
+//  Copyright © 2020 rahardyan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
