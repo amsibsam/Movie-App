c761d41577727a1d7bb29746bc2f7f335aaf0603
Rahardyan Bisma <rahardyanbismasetya.putra@mitrais.com>
2020-09-02 13:53:21 +0700

    add title on moviesViewController

--- MovieApp/Scene/Genres/GenresViewController.swift
+++ MovieApp/Scene/Genres/GenresViewController.swift
@@ -32,6 +32,16 @@ class GenresViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.isNavigationBarHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.navigationController?.isNavigationBarHidden = false
+    }
+    
     private func bindView() {
         viewModel.refreshDriver.drive(onNext: { [weak self] (_) in
             self?.tableViewGenre.reloadData()
